a7cb1d37de8be22c6e4d8d737df69223b6216cdf
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-07-19 05:47:49 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,10 +10,6 @@ LOCAL_PATH := device/tecno/LH8n
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Virtual A/B
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
