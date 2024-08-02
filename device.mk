cad206c78a94778a8068e21ac952cbb55aae2fb3
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-02 16:57:43 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -51,17 +51,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
-# Additional Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libkeymaster41 \
-    libpuresoftkeymasterdevice
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-
 # MTK plpath utils
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
