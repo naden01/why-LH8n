ea255b0b29e0bf6c709c6631426a29f39784df1c
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-02 09:23:41 -0700

    Update twrp_LH8n.mk

--- twrp_LH8n.mk
+++ twrp_LH8n.mk
@@ -9,6 +9,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
+# # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
@@ -20,5 +23,3 @@ PRODUCT_NAME := twrp_LH8n
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LH8n
 PRODUCT_MANUFACTURER := tecno
-
-PRODUCT_GMS_CLIENTID_BASE := android-tecno
