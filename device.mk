59feca1c9826459979c55133350bf79b401e6695
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-04 04:08:37 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,7 +13,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
